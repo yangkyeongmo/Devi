cf43fb276f79a4d98d5e3f3d934013b5b049242f
yangkyeongmo <yangkyeongmo@snu.ac.kr>
2018-02-12 19:53:29 +0900

    earth shoots missile

---
+++ Scripts/EarthReactions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthReactions : MonoBehaviour {
+
+    public Transform[] launchPosition;
+    public GameObject attackMissile;
+    public int missileSpawnInterval;
+
+    private Transform selectedLaunchPosition;
+    private int timeNow;
+    private int timeNext;
+
+	// Use this for initialization
+	void Start () {
+        timeNow = (int)Time.time;
+        timeNext = timeNow;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeNow = (int)Time.time;
+        if (timeNow == timeNext)
+        {
+            timeNext = timeNow + missileSpawnInterval;
+            SpawnMissile();
+        }
+	}
+
+    void SpawnMissile()
+    {
+        Vector3 direction;
+        int randomCount = Random.Range(0, launchPosition.Length - 1);
+        selectedLaunchPosition = launchPosition[randomCount];
+        Debug.Log("Attack Missile Launch at : Position" + randomCount);
+        direction = (transform.position - selectedLaunchPosition.position).normalized;
+
+        GameObject spawnedMissile = Instantiate(attackMissile, selectedLaunchPosition.position , Quaternion.identity);
+        spawnedMissile.transform.up = direction;
+        Debug.Log("Attack Missile Launched");
+    }
+}
---
+++ Scripts/MoveMissile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMissile : MonoBehaviour {
+
+    public float initialSpeed;
+    public float modifier;
+    public GameObject marker;
+
+    private Vector3 playerPosition;
+    private Rigidbody rb;
+    private float speed;
+    private Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.up * initialSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerPosition = GameObject.FindWithTag("Player").transform.position;
+        speed = rb.velocity.magnitude;
+        direction = (transform.up + (playerPosition - transform.position).normalized * modifier).normalized;
+        transform.up = direction;
+        rb.velocity = speed * direction;
+        GameObject mark = Instantiate(marker, transform);
+        mark.transform.parent = GameObject.Find("StaticParent").transform;
+    }
+}
