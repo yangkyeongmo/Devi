f40aed42f3d71a25d72789f679d4904f589f65bd
yangkyeongmo <yangkyeongmo@snu.ac.kr>
2018-02-22 19:13:12 +0900

    move shot, destroy shot on collision

--- Scripts/MoveShot.cs
+++ Scripts/MoveShot.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class MoveShot : MonoBehaviour {
 
+    public float speed;
+
+    private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        rb.velocity = transform.up * speed;
 	}
-    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+        //decrease health of collision.gameobject
+    }
 }
