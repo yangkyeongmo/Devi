5756f8b55f64e1068ab99f765da04dc2f3680862
yangkyeongmo <yangkyeongmo@snu.ac.kr>
2018-02-22 20:28:20 +0900

    removed MoveShot/shoot every 'shootInterval' seconds

--- Scripts/TurretShoot.cs
+++ Scripts/TurretShoot.cs
@@ -5,17 +5,43 @@ using UnityEngine;
 public class TurretShoot : MonoBehaviour {
 
     public GameObject shot;
+    public float shootInterval;
+    public float shotSpeed;
+
+    private bool spacePressed;
+    private float firstValue, nextValue;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
-            GameObject instshot = Instantiate(shot, transform.position + transform.up * 35, Quaternion.LookRotation(transform.up));
+            spacePressed = true;
+            if (spacePressed)
+            {
+                if(Time.time >= nextValue)
+                {
+                    Shoot();
+                    nextValue += shootInterval;
+                }
+            }
+            Debug.Log("Called after shooting");
         }
-	}
+        else if (Input.GetKey(KeyCode.Space) != true)
+        {
+            spacePressed = false;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject instshot = Instantiate(shot, transform.position + transform.up * 3.5f, Quaternion.LookRotation(transform.up));
+        instshot.transform.up = transform.up;
+        Rigidbody rb = instshot.GetComponent<Rigidbody>();
+        rb.velocity = transform.up * shotSpeed;
+    }
 }
