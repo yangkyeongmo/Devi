bba49e23def467c64cdf54e8f43f3575c1a78b90
yangkyeongmo <yangkyeongmo@snu.ac.kr>
2018-02-23 20:47:31 +0900

    added HijackerScript

---
+++ Scripts/HijackerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HijackerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
