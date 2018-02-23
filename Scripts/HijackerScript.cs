2341d1878fc1c4ba13766849ffe36c6b26fba52f
yangkyeongmo <yangkyeongmo@snu.ac.kr>
2018-02-23 21:03:46 +0900

    hijacker redirects enemy

--- Scripts/HijackerScript.cs
+++ Scripts/HijackerScript.cs
@@ -4,13 +4,58 @@ using UnityEngine;
 
 public class HijackerScript : MonoBehaviour {
 
+    public float redirectionSpeed;
+
+    private List<GameObject> enemies;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        enemies = new List<GameObject>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Redirection();
+            //DestroyEnemies();
+        }
 	}
+
+    void Redirection()
+    {
+        if (enemies.Count != 0)
+        {
+            foreach (GameObject enemy in enemies)
+            {
+                Rigidbody rb = enemy.GetComponent<Rigidbody>();
+                rb.velocity += (enemy.transform.position - transform.position).normalized * redirectionSpeed;
+            }
+        }
+    }
+
+    void DestroyEnemies()
+    {
+        if (enemies.Count != 0)
+        {
+            foreach (GameObject enemy in enemies)
+            {
+                Destroy(enemy.gameObject);
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        enemies.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (enemies.Contains(other.gameObject))
+            enemies.Remove(other.gameObject);
+    }
 }
